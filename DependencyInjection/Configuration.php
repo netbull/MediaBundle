7e1dea63ac6827d8a63083c3cf23c07f2362a9e4
Aleksandar Dimitrov <netbull2007@gmail.com>
2019-05-30 13:02:11 +0300

    Update

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('netbull_media');
+        $treeBuilder = new TreeBuilder('netbull_media');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
