c60819560dfd43273c91b780eed949bf95452814
Aleksandar Dimitrov <netbull2007@gmail.com>
2021-02-09 11:10:51 +0200

    fix: YouTube metadata download

--- src/Provider/YouTubeProvider.php
+++ src/Provider/YouTubeProvider.php
@@ -259,7 +259,7 @@ class YouTubeProvider extends BaseVideoProvider
      */
     public function updateMetadata(MediaInterface $media, $force = false)
     {
-        $url = sprintf('http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=%s&format=json', $media->getProviderReference());
+        $url = sprintf('https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=%s&format=json', $media->getProviderReference());
 
         try {
             $metadata = $this->getMetadata($url);
